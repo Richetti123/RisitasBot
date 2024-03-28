7588f174394c681e123c1f153bd1f86904ddfb2b
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-03-28 03:31:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { en, es, id, ar, pt, fr, hi } from "./lib/idiomas/total-idiomas.js"
 global.owner = [
 ["56941827124", 'Richetti', true], //Richetti
 ["51966254634"], 
-["56945086055"], 
+["56962386185"], 
 ["56945086055"], 
 ["56945086055"],
 ["56945086055"],
@@ -103,26 +103,26 @@ global.moment = moment
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.official = [ // Agregate si eres Owner
-["56945086055", 'MichiBot', 1], 
-["56945086055", 'MichiBot', 1],  
-["56945086055", 'MichiBot', 1],
-["56945086055", 'MichiBot', 1],
-["56945086055", 'MichiBot', 1]]
+["56945086055", 'RisitasBot', 1], 
+["56945086055", 'RisitasBot', 1],  
+["56945086055", 'RisitasBot', 1],
+["56945086055", 'RisitasBot', 1],
+["56945086055", 'RisitasBot', 1]]
 
 global.mail = '' //agrega tú correo
-global.desc = 'MichiBot el mejor bot de whatsapp' //agrega una descripción corta
-global.desc2 = 'MichiBot el mejor bot de whatsapp' //agrega una descripción larga (Solo se aplicará si su whasapp no tiene descripción)
+global.desc = 'RisitasBot el mejor bot de whatsapp' //agrega una descripción corta
+global.desc2 = 'RisitasBot el mejor bot de whatsapp' //agrega una descripción larga (Solo se aplicará si su whasapp no tiene descripción)
 global.country = 'Perú' //agrega tú país ejemplo: 🇪🇨
 
-global.packname = "MichiBot"
-global.author = "MichiBot"
+global.packname = "RisitasBot"
+global.author = "RisitasBot"
 
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
 
-global.gt = "MichiBot"
+global.gt = "RisitasBot"
 global.yt = "https://www.instagram.com/ceogeripium.dzn"
 global.yt2 = "https://www.instagram.com/ceogeripium.dzn"
 global.ig = "https://www.instagram.com/ceogeripium.dzn"
@@ -177,8 +177,8 @@ global.exito = eg
 
 //⊱ ━━━━━.⋅ Información | Information ⋅.━━━━ ⊰
 
-global.wm = "MichiBot"
-global.igfg = "MichiBot"
+global.wm = "RisitasBot"
+global.igfg = "RisitasBot"
 global.wait = "*⌛ _Cargando | Charging..._ ▬▭▭▭▭▭▭*"
 global.waitt = "*⌛ _Cargando | Charging..._ ▬▬▭▭▭*"
 global.waittt = "*⌛ _Cargando | Charging..._ ▬▬▬▬▭▭*"
